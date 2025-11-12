29e1b2ecc65dc32028d2b1c718aed395cf53ca5d
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-12 14:10:13 +0800

    refactor: Improve About section layout and styling

--- src/AboutSection.tsx
+++ src/AboutSection.tsx
@@ -7,8 +7,8 @@ import 'react-lazy-load-image-component/src/effects/opacity.css';
 export default function AboutSection() {
   return (
     <section id='About'>
-      <div className='flex flex-col lg:flex-col xl:flex-row m-0 md:m-30 lg:m-40 border-y-black shadow-xl bg-gray-900 bg-opacity-70 px-8 py-5 md:px-20 lg:px-20'>
-        <div className='flex flex-col flex-grow-0'>
+      <div className='flex flex-col lg:flex-row m-0 sm:m-3 md:m-30 lg:m-40 border-y-black shadow-xl bg-gray-900 bg-opacity-70 px-8 py-5 md:px-20 lg:px-20'>
+        <div className='flex flex-col flex-grow'>
           <div className='myProfileImg flex flex-1 justify-center'>
             <LazyLoadImage
               effect='blur'
@@ -17,77 +17,75 @@ export default function AboutSection() {
                 style: { transitionDelay: '0.3s' }, // Adjust transition delay
               }}
               src={profile2}
-              className='profile1'
-              width='340'
-              height='340'
-              alt=''
+              className='profile1 object-cover h-96'
+              alt='Profile'
             />
           </div>
-          <div className='mysocialMedia flex flex-col justify-center space-y-4 mx-16 my-10'>
+          <div className='mysocialMedia flex flex-row lg:flex-col my-10 gap-4'>
             <div className='flex flex-1 group'>
-              <i className='ri-github-fill text-[30px] text-white group-hover:text-pink-700'></i>
+              <i className='ri-github-fill text-3xl text-white group-hover:text-pink-700'></i>
               <a
                 href='https://github.com/reqbahrf'
                 target='_blank'
-                className='text-white ps-5 group-hover:text-pink-700'
+                className='text-white hidden md:block ps-5 group-hover:text-pink-700'
               >
                 GitHub
               </a>
             </div>
             <div className='flex flex-1 group'>
-              <i className='ri-figma-fill text-[30px] text-white group-hover:text-pink-700'></i>
+              <i className='ri-figma-fill text-3xl text-white group-hover:text-pink-700'></i>
               <a
                 href='https://www.figma.com/@reanzarthuramon'
                 target='_blank'
-                className='text-white ps-5 group-hover:text-pink-700'
+                className='text-white hidden md:block ps-5 group-hover:text-pink-700'
               >
                 Figma
               </a>
             </div>
             <div className='flex flex-1 group'>
-              <i className='ri-behance-fill text-[30px] text-white group-hover:text-pink-700'></i>
+              <i className='ri-behance-fill text-3xl text-white group-hover:text-pink-700'></i>
               <a
                 href='https://www.behance.net/reanzamonera'
                 target='_blank'
-                className='text-white ps-5 group-hover:text-pink-700'
+                className='text-white hidden md:block ps-5 group-hover:text-pink-700'
               >
                 Behance
               </a>
             </div>
             <div className='flex flex-1 group'>
-              <i className='ri-facebook-fill text-[30px] text-white group-hover:text-pink-700'></i>
+              <i className='ri-facebook-fill text-3xl text-white group-hover:text-pink-700'></i>
               <a
                 href='https://www.facebook.com/profile.php?id=100008998324688'
                 target='_blank'
-                className='text-white ps-5 group-hover:text-pink-700'
+                className='text-white hidden md:block ps-5 group-hover:text-pink-700'
               >
                 Facebook
               </a>
             </div>
             <div className='flex flex-1 group'>
-              <i className='ri-instagram-line text-[30px] text-white group-hover:text-pink-700'></i>
+              <i className='ri-instagram-line text-3xl text-white group-hover:text-pink-700'></i>
               <a
                 href='https://www.instagram.com/reanz_arthur/'
                 target='_blank'
-                className='text-white ps-5 group-hover:text-pink-700'
+                className='text-white hidden md:block ps-5 group-hover:text-pink-700'
               >
                 Instagram
               </a>
             </div>
-            <hr />
-            <div className='flex flex-1 group'>
-              <i className='ri-mail-fill text-[30px] text-white group-hover:text-pink-700'></i>
-              <a
-                href='mailto:reqbahrf@gmail.com'
-                className='text-white ps-5 group-hover:text-pink-700'
-              >
-                reqbahrf@gmail.com
-              </a>
-            </div>
+          </div>
+          <hr />
+          <div className='flex flex-1 group mt-5 lg:mt-0 justify-center lg:justify-start'>
+            <i className='ri-mail-fill text-3xl text-white group-hover:text-pink-700'></i>
+            <a
+              href='mailto:reqbahrf@gmail.com'
+              className='text-white ps-5 group-hover:text-pink-700'
+            >
+              reqbahrf@gmail.com
+            </a>
           </div>
         </div>
-        <div className='intro flex flex-col flex-grow w-auto m-5 md:m-12'>
-          <div className='text-white text-3xl font-semibold my-4'>
+        <div className='intro flex flex-col flex-grow w-auto m-5 md:m-12 lg:m-18'>
+          <div className='text-white text-xl md:text-3xl font-semibold my-4'>
             I&apos;m Reanz Arthur Antone Monera
           </div>
           <div className='text-white text-lg mb-7 indent-8 text-justify'>
