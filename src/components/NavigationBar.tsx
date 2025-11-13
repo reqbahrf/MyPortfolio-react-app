15b33372852bd0d474744ee168a640b5931753de
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-13 16:10:35 +0800

    feat: Add social media links to NavigationBar and update styling

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,5 +1,7 @@
 import {
   RiHomeLine,
+  RiGithubFill,
+  RiLinkedinBoxFill,
   RiUserLine,
   RiFolderLine,
   RiAwardLine,
@@ -11,40 +13,67 @@ const NavigationBar = () => {
       <div className='bg-white/80 backdrop-blur-lg w-[95dvw] md:w-auto rounded-full h-10'>
         <nav
           id='nav-links'
-          className='flex justify-center gap-2 sm:gap-4 md:gap-6 h-full'
+          className='flex justify-center gap-1 hover:gap-2 sm:gap-3 sm:hover:gap-4 md:hover:gap-5 h-full transition-all ease-in-out duration-300'
         >
           <a
             href='#Home'
-            className='relative group hover:text-pink-700 px-4 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all'
+            className='relative group px-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-6 hover:rounded-full hover:bg-pink-700 hover:text-white'
             title='Home'
           >
-            <RiHomeLine className='ri-home-line text-xl' />
+            <RiHomeLine className='text-xl' />
             <span className='nav-bar-text'>Home</span>
           </a>
-          <div className='border-r-zinc-950 border-r -ms-7'></div>
+          <div className='border-r-zinc-950 border-r -ms-2'></div>
           <a
             href='#About'
-            className='relative group hover:text-pink-700 px-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all'
+            className='relative group  pe-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
             title='About'
           >
-            <RiUserLine className='ri-user-line text-xl' />
+            <RiUserLine className='text-xl' />
             <span className='nav-bar-text'>About</span>
           </a>
           <a
             href='#Project'
-            className='relative group hover:text-pink-700 px-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all'
+            className='relative group px-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
             title='Projects'
           >
-            <RiFolderLine className='ri-folder-line text-xl' />
+            <RiFolderLine className=' text-xl' />
             <span className='nav-bar-text'>Projects</span>
           </a>
           <a
             href='#Certificates'
-            className='relative group hover:text-pink-700 px-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all'
+            className='relative group px-2 py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
             title='Certificates'
           >
-            <RiAwardLine className='ri-award-line text-xl' />
+            <RiAwardLine className='text-xl' />
             <span className='nav-bar-text'>Certificates</span>
+          </a>
+          <div className='border-r-zinc-950 border-r -me-2'></div>
+          <a
+            href='https://github.com/reqbahrf'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='relative group py-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            title='Github'
+          >
+            <RiGithubFill
+              size={25}
+              className=' text-xl'
+            />
+            <span className='nav-bar-text'>Github</span>
+          </a>
+          <a
+            href='https://www.linkedin.com/in/reanz-arthur-monera-b20b89350'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='relative group py-2 pe-2 text-gray-800 flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            title='Linkedin'
+          >
+            <RiLinkedinBoxFill
+              size={25}
+              className=' text-xl'
+            />
+            <span className='nav-bar-text'>Linkedin</span>
           </a>
         </nav>
       </div>
