e752b60724b5136c49df1cae16f6a9554173462d
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-14 00:54:32 +0800

    Refactor: Improve NavigationBar styling and transitions

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -15,20 +15,20 @@ const NavigationBar = () => {
       <div className='bg-white/80 dark:bg-black/80 backdrop-blur-lg w-auto rounded-full h-10'>
         <nav
           id='nav-links'
-          className='flex justify-center gap-1 hover:gap-2 sm:gap-3 sm:hover:gap-4 md:hover:gap-5 h-full transition-all ease-in-out duration-300'
+          className='flex justify-center gap-1 hover:gap-2 h-full transition-transform ease-in-out duration-300'
         >
           <a
             href='#Home'
-            className='relative group ps-2 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-6 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group px-2 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Home'
           >
             <RiHomeLine className='text-xl' />
             <span className='nav-bar-text'>Home</span>
           </a>
-          <div className='border-r-zinc-950 dark:border-r-zinc-50 border-r -ms-2'></div>
+          <div className='border-r-zinc-950 dark:border-r-zinc-500 border-r -ms-1'></div>
           <a
             href='#Project'
-            className='relative group px-1 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group px-1 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Projects'
           >
             <RiFolderLine className=' text-xl' />
@@ -36,18 +36,18 @@ const NavigationBar = () => {
           </a>
           <a
             href='#Certificates'
-            className='relative group px-1 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group px-1 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Certificates'
           >
             <RiAwardLine className='text-xl' />
             <span className='nav-bar-text'>Certificates</span>
           </a>
-          <div className='border-r-zinc-950 dark:border-r-zinc-50 border-r -me-2'></div>
+          <div className='border-r-zinc-950 dark:border-r-zinc-500 border-r -me-1'></div>
           <a
             href='https://github.com/reqbahrf'
             target='_blank'
             rel='noopener noreferrer'
-            className='relative group ps-1 py-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group px-1 py-2 text-gray-800 dark:text-white flex items-center justify-center transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Github'
           >
             <RiGithubFill
@@ -60,7 +60,7 @@ const NavigationBar = () => {
             href='https://www.linkedin.com/in/reanz-arthur-monera-b20b89350'
             target='_blank'
             rel='noopener noreferrer'
-            className='relative group py-2 px-1 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group py-2 px-1 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Linkedin'
           >
             <RiLinkedinBoxFill
@@ -73,7 +73,7 @@ const NavigationBar = () => {
             href='https://www.figma.com/@reanzarthuramon'
             target='_blank'
             rel='noopener noreferrer'
-            className='relative group py-2 px-1 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group py-2 px-1 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Figma'
           >
             <RiFigmaFill
@@ -86,7 +86,7 @@ const NavigationBar = () => {
             href='https://www.facebook.com/reanz.arthur.antone.monera/'
             target='_blank'
             rel='noopener noreferrer'
-            className='relative group py-2 pe-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+            className='relative group py-2 px-1 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white'
             title='Facebook'
           >
             <RiFacebookBoxFill
@@ -95,7 +95,7 @@ const NavigationBar = () => {
             />
             <span className='nav-bar-text'>Facebook</span>
           </a>
-          <div className='border-r-zinc-950 dark:border-r-zinc-50 border-r -ms-2'></div>
+          <div className='border-r-zinc-950 dark:border-r-zinc-500 border-r -ms-1'></div>
           <ThemeToggle />
         </nav>
       </div>
