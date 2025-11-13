eae39c400a81c72bb3bee04b9b1df202a4e4b2cd
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-14 00:54:32 +0800

    Feat: Enhance tooltip animation and styling

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -6,7 +6,7 @@ const ThemeToggle = () => {
     <button
       title='Toggle Theme'
       onClick={toggleTheme}
-      className='relative group py-2 pe-2 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-all ease-in-out duration-300 hover:px-4 hover:rounded-full hover:bg-pink-700 hover:text-white'
+      className='relative group py-2 px-1 text-gray-800 dark:text-white flex items-center justify-center hover:scale-125 transition-transform ease-in-out duration-300  rounded-full hover:bg-pink-700 hover:text-white hover:shadow-lg hover:shadow-pink-700/50'
     >
       {theme ? <RiSunFill /> : <RiMoonFill />}
       <span className='nav-bar-text'>Toggle Theme</span>
