4a1ccc8ab6f41a1b97f174ea3c938137422a8e04
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-16 14:59:32 +0800

    Style: Update PillTag to use darker background for dark mode

--- src/components/PillTag.tsx
+++ src/components/PillTag.tsx
@@ -6,7 +6,7 @@ interface PillTagProps {
 
 const PillTag: React.FC<PillTagProps> = ({ tag }) => {
   return (
-    <span className='bg-gray-700 text-white text-sm font-semibold px-2 py-1 rounded-sm dark:bg-gray-200 dark:text-black'>
+    <span className='dark:bg-gray-900 dark:text-white text-sm font-semibold px-2 py-1 rounded-sm bg-gray-200 text-black'>
       {tag}
     </span>
   );
