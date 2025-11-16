73722dd167c608232c6fef9945f840054e109d0a
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-16 14:59:33 +0800

    Style: Adjust skeleton component dimensions for better visual consistency

--- src/components/skeleton/chart/HeatMap.tsx
+++ src/components/skeleton/chart/HeatMap.tsx
@@ -1,6 +1,6 @@
 const HeatMap = () => {
   return (
-    <div className='bg-gray-700 h-full w-full rounded-sm animate-pulse'></div>
+    <div className='bg-gray-700 w-[95dvw] sm:w-[85dvw] md:w-[70dvw] lg:w-[60dvw] min-h-[500px] rounded-sm animate-pulse'></div>
   );
 };
 
