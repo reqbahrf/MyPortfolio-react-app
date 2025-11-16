d62ef3c368ae3119c10a89b83a738633b648b47b
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-16 13:21:23 +0800

    feat: Add dedicated skeleton components for charts

---
+++ src/components/skeleton/chart/Donut.tsx
@@ -0,0 +1,7 @@
+const Donut = () => {
+  return (
+    <div className='bg-gray-700 h-64 w-64 rounded-full animate-pulse mt-4'></div>
+  );
+};
+
+export default Donut;
---
+++ src/components/skeleton/chart/HeatMap.tsx
@@ -0,0 +1,5 @@
+const HeatMap = () => {
+  return <div className='bg-gray-700 h-10 w-24 rounded-sm animate-pulse'></div>;
+};
+
+export default HeatMap;
