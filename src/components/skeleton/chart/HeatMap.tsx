ed645ecfe8acc7f474453adc8fc5863f8385afcd
Reanz_Arthur <reqbahrf@gmail.com>
2025-11-16 13:27:34 +0800

    Feat: Enhance HeatMap skeleton component styling

--- src/components/skeleton/chart/HeatMap.tsx
+++ src/components/skeleton/chart/HeatMap.tsx
@@ -1,5 +1,7 @@
 const HeatMap = () => {
-  return <div className='bg-gray-700 h-10 w-24 rounded-sm animate-pulse'></div>;
+  return (
+    <div className='bg-gray-700 h-full w-full rounded-sm animate-pulse'></div>
+  );
 };
 
 export default HeatMap;
